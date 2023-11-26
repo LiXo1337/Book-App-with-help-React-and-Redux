c91b618cb9fca7366b5aa6a4687ca13bcdf9bb8b
Sergei Gybich <sergeigybich12@gmail.com>
2023-11-26 19:07:31 +0200

    Added isLoading

--- frontend/src/components/BookFrom/BookForm.jsx
+++ frontend/src/components/BookFrom/BookForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addBook, fetchBook } from "../../redux/slices/bookSlice";
 import bookData from "../../data/book.json";
+import { FaSpinner } from "react-icons/fa";
 import "./BookForm.css";
 import { createBookWithId } from "../../utils/createBookWithId";
 import { setError } from "../../redux/slices/errorSlice";
@@ -9,6 +10,7 @@ import { setError } from "../../redux/slices/errorSlice";
 const BookForm = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
   const handleAddRandomBook = () => {
@@ -19,8 +21,13 @@ const BookForm = () => {
     dispatch(addBook(randomBookWithId));
   };
 
-  const handleAddRandomBookViaAPI = () => {
-    dispatch(fetchBook("http://localhost:4000/random-book"));
+  const handleAddRandomBookViaAPI = async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchBook("http://localhost:4000/random-book"));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -61,8 +68,20 @@ const BookForm = () => {
         <button type="button" onClick={() => handleAddRandomBook()}>
           Add Random
         </button>
-        <button type="button" onClick={handleAddRandomBookViaAPI}>
-          Add Random via API
+
+        <button
+          type="button"
+          onClick={handleAddRandomBookViaAPI}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <span>Loading Book...</span>
+              <FaSpinner className="spinner" />
+            </>
+          ) : (
+            "Add Random via API"
+          )}
         </button>
       </form>
     </div>
