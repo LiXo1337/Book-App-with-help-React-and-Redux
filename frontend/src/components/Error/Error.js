a25731b29ab05846b238b27df6c75b591e1595f0
Sergei Gybich <sergeigybich12@gmail.com>
2023-11-26 17:41:46 +0200

    Update error

---
+++ frontend/src/components/Error/Error.js
@@ -0,0 +1,24 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearError,
+  selectorErrorMassege,
+} from "../../redux/slices/errorSlice";
+import { useEffect } from "react";
+
+const Error = () => {
+  const errorMassege = useSelector(selectorErrorMassege);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (errorMassege) {
+      toast.info(errorMassege);
+      dispatch(clearError);
+    }
+  }, [errorMassege, dispatch]);
+
+  return <ToastContainer position="top-right" autoClose={2000} />;
+};
+
+export default Error;
